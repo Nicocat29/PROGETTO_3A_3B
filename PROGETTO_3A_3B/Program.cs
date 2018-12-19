7b84b9711330a5d5b3d2f25950a08fa427c57280
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-19 08:43:08 +0100

    abbiamo creato un programma che converte un numero a scelta tra 3 diverse basi, (2, 8, 16)

--- PROGETTO_3A_3B/Program.cs
+++ PROGETTO_3A_3B/Program.cs
@@ -11,18 +11,34 @@ namespace PROGETTO_3A_3B
         static void Main(string[] args)
         {
             string risultato = "";
-            int num = 255;
-            Console.WriteLine("inserisci un numero da convertire in binario");
-            num = Convert.ToInt32(Console.ReadLine());
-            
-            while(num>1)
+            // Inizializiamo
+            int num = 0,basi = 0, resto=0;
+            do
             {
-                int resto =num % 2;
-                risultato = Convert.ToString(resto) + risultato;
-                num = num / 2;
+                // Inseriamo l'Input e l'Output che ci chiede di inserire il numero
+                Console.WriteLine("inserisci un numero da convertire ");
+                num = Convert.ToInt32(Console.ReadLine());
+            } while (num < 0);
+            do
+            {
+                Console.WriteLine("inserisci un numero da convertire tra 2 8 o 16");
+               basi = Convert.ToInt32(Console.ReadLine());
+            } while (basi != 2 && basi != 8 && basi != 16);
+
+            while (num>1)
+            {
+                resto = num % basi;
+                risultato=Convert.ToString(resto, basi) + risultato;
+                num = num / basi;
             }
-            risultato = Convert.ToString(num) + risultato;
-            Console.WriteLine($"il numero binario è{risultato}");
+
+            risultato = Convert.ToString(resto, basi) + risultato;
+            Console.WriteLine($"il umero convertito in base {basi} è {risultato}");
+            Console.WriteLine("");
+
+           
+
+            Console.ReadLine();
 
 
 
